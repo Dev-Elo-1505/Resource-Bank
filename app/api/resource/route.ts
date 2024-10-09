4c460b04919fb6cc36b399ec5bda51fdaab2bf2e
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-10-09 19:03:05 +0100

    set up POST request

--- app/api/resource/route.ts
+++ app/api/resource/route.ts
@@ -1,6 +1,29 @@
-import { NextRequest } from "next/server"
-import { resourceSchema } from "@/app/validationSchema"
+import { NextRequest, NextResponse } from "next/server";
+import { resourceSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = resourceSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
 
+  try {
+    const newResource = await prisma.resource.create({
+      data: {
+        title: body.title,
+        description: body.description,
+        url: body.url,
+        category: body.category,
+      },
+    });
+    return NextResponse.json(newResource, { status: 201 });
+  } catch (error) {
+    console.error("Error creating resource", error);
+    return NextResponse.json(
+      { error: "Failed to create resource." },
+      { status: 500 }
+    );
+  }
 }
