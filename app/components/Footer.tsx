6e3b87446d771c2edb130a0ebbed505ea372abc1
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2024-10-09 17:35:47 +0100

    Build desktop footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return <footer className="px-20 py-5 bg-black text-center text-white absolute w-full bottom-0">
+    <p>Designed & Developed by <a className="hover:text-primary cursor-pointer">Elo-oghene</a> © Copyright 2024</p>
+  </footer>;
 };
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative min-h-screen">
       <Header />
       <div className="text-green-900 underline">Hello Resource</div>
+      <Footer />
     </div>
   );
 }
